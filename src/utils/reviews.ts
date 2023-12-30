ba36395c0b5061cd3ba95646e8a0c325c80b713a
matias_cabrera <matiasc.02.2003@gmail.com>
2023-12-30 01:39:39 -0300

    FEAT: avatar convert to webp

--- src/utils/reviews.ts
+++ src/utils/reviews.ts
@@ -3,24 +3,24 @@ export const reviews = [
         id : 1,
         name : 'Anisha Li',
         review : '"Manage has supercharged our team´s workflow. The ability to maintan visibility on larger milestones at all times keeps everyone motivated."',
-        avatar : '/images/avatar-anisha.png'
+        avatar : '/images/avatar-anisha.webp'
     },
     {
         id : 2,
         name : 'Ali Bravo',
         review : '"We have been able to cancel so many other subscriptions since using Manager. There is no more corss-channel confusion and everyone is much more focused."',
-        avatar : '/images/avatar-ali.png'
+        avatar : '/images/avatar-ali.webp'
     },
     {
         id : 3,
         name : 'Richard Watts',
         review : '"Manage allows us to provide strucutre and process. it keeps us organized and focused. i can´t stop recommending them to everyone i talk to!',
-        avatar : '/images/avatar-richard.png'
+        avatar : '/images/avatar-richard.webp'
     },
     {
         id : 4,
         name : 'Shanai Gough',
         review : '"Their software allows us to track, manage and collaborate on our projects form anywhere. it keeps the whole team in-sync without being intrusive."',
-        avatar : '/images/avatar-shanai.png'
+        avatar : '/images/avatar-shanai.webp'
     }
 ]
